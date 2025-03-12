80d42d8f98b218f36acf0d2fb665b15e374f2ce7
tramiNGY <ngytrami@gmail.com>
2025-03-12 12:00:20 +0100

    In put of PlaceRessource class deleted is_admin

--- part3/hbnb/app/api/v1/places.py
+++ part3/hbnb/app/api/v1/places.py
@@ -77,10 +77,8 @@ class PlaceResource(Resource):
         # check if the place exists in the database
         if not place:
             return {'error': 'Place not found'}, 404
-        # Check if the current user is the owner or admin
-        is_admin = current_user_id.get('is_admin', False)
         # only the owner of the place can modify its information
-        if place.owner != current_user_id and not is_admin:
+        if place.owner != current_user_id:
             return {'error': 'Unauthorized action'}, 403
         user_place = api.payload
         # updates the place information in the database
@@ -137,4 +135,3 @@ class AdminPlaceModify(Resource):
         if not updated_place:
             return {'error': 'Failed to update this place'}, 500
         return {'id': updated_place.id, 'title': updated_place.title, 'description': updated_place.description, 'price': updated_place.price, 'latitude': updated_place.latitude, 'longitude' : updated_place.longitude, 'amenities' : updated_place.amenities}, 200
-    
