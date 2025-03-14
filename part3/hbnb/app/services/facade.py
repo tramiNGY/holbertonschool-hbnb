85242486d39b9aa72dd3ac608be26e8bac6f0a7e
raphaeldott <raphael.dott@hotmail.com>
2025-03-14 16:35:59 +0100

    import for review and amenity in facade

--- part3/hbnb/app/services/facade.py
+++ part3/hbnb/app/services/facade.py
@@ -5,6 +5,8 @@ from app.models.place import Place
 from app.models.review import Review
 from app.services.repositories.user_repository import UserRepository
 from app.services.repositories.place_repository import PlaceRepository
+from app.services.repositories.review_repository import ReviewRepository
+from app.services.repositories.amenity_repository import AmenityRepository
 
 class HBnBFacade:
     """Class for facade methods"""
