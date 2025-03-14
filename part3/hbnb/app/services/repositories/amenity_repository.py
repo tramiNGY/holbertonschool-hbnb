7bd6d3cd9210e20534f788da3ee824b27c1c16b3
raphaeldott <raphael.dott@hotmail.com>
2025-03-14 16:37:55 +0100

    removed useless method for amenity repo

--- part3/hbnb/app/services/repositories/amenity_repository.py
+++ part3/hbnb/app/services/repositories/amenity_repository.py
@@ -6,6 +6,3 @@ class AmenityRepository(SQLAlchemyRepository):
     def __init__(self):
         super().__init__(Amenity)
 
-    def get_amenity_by_name(self, name):
-        # Retrieve an amenity by name
-        return self.model.query.filter_by(name=name).first()
